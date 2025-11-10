7c61ee9ddd0e7d76e0b69411cca2a630c48ac8bc
[Caleb Wheeler] <[cefourwheeler@gmail.com]>
2025-11-10 15:15:22 -0500

    done

--- js/hotel.js
+++ js/hotel.js
@@ -1,29 +1,34 @@
 // test
-//var guest = {
-    //name: "Caleb",
-    //age : 19,
-    //date : "10/29/25",
-    //otherguestsNames : ["Tim", "Jim"],
-    //display: function() {
-        //return this.name + " " + this.age + " " + this.date + " " + this.otherguestsNames;
-    //}
-//};
-
-var guestgroup = new Array();
-//console.log(guest.display());
-//hotel registration
-var guest={
-    name: document.getElementById("guestname").value,
-    checkin: document.getElementById("checkin").value,
-    checkout: document.getElementById("checkout").value,
-    numguest: document.getElementById("guestnum").value,
-    roomtype: document.getElementById("room").value
+var guest1 = {
+    name: "Caleb",
+    age : 19,
+    date : "10/29/25",
+    otherguestsNames : ["Tim", "Jim"],
+    display: function() {
+        return this.name + " " + this.age + " " + this.date + " " + this.otherguestsNames;
+    }
 };
-guestgroup.push(guest);
-console.log(guestgroup);
 
-document.getElementById("submit").addEventListener("click", reservation);
+console.log(guest1.display());
+// hotel registration
+var guestgroup = [];
 
-function reservation() {
-
+function reservation(){
+    var guest = {
+        name: document.getElementById('guestname').value,
+        checkin: document.getElementById('checkin').value,
+        checkout: document.getElementById('checkout').value,
+        numguest: document.getElementById('guestnum').value,
+        roomtype: document.getElementById('room').value
+    };
+    guestgroup.push(guest);
+    console.log('guestgroup', guestgroup);
+    alert('Reservation added:\n'+"guest name: " + guest.name + ', guest roomtype: ' + guest.roomtype + ', guest checkin: ' + guest.checkin + ' to ' + guest.checkout);
 }
+
+document.getElementById('lists').addEventListener('click', function(){
+    if(guestgroup.length===0){ alert('No reservations yet'); return; }
+    alert(JSON.stringify(guestgroup, null, 2));
+}, false);
+
+document.getElementById('submit').addEventListener('click', reservation, false);
